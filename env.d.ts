3c6d63ec67b68842ed336166fb756f9fcd1f788f
Salman Bashar <salman@lemonhive.com>
2025-01-22 20:47:57 +0600

    refactor: update env

--- env.d.ts
+++ env.d.ts
@@ -3,8 +3,7 @@
 
 interface ImportMetaEnv {
   readonly SANITY_STUDIO_PROJECT_ID: string;
-  readonly SANITY_STUDIO_PREVIEW_URL_DEVELOP: string;
-  readonly SANITY_STUDIO_PREVIEW_URL_PRODUCTION: string;
+  readonly SANITY_STUDIO_PREVIEW_URL: string;
   readonly SANITY_STUDIO_PREVIEW_SECRET: string;
   // more env variables...
 }
